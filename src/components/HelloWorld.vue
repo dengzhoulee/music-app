d0388e6caa3b3277cb32a26544cd25cd85546663
l-d-z <dengzhouli@163.com>
2018-03-14 11:16:21 +0800

    add sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div.hello
+  .hello
     h1 {{ msg }}
     h2 Essential Links
     ul
@@ -36,23 +36,16 @@
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+<style scoped lang="sass" rel="stylesheet/sass">
+  .hello
+    h1, h2
+      font-weight: normal
+    ul
+      list-style-type: none
+      padding: 0
+      li
+        display: inline-block
+        margin: 0 10px
+        a
+          color: #42b983
 </style>
