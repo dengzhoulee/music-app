f32d2b9007d3904dfb19b0d3fdfa81b4706e6a07
l-d-z <dengzhouli@163.com>
2018-03-14 11:02:28 +0800

    add pug template

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,12 @@ module.exports = {
     parser: 'babel-eslint'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/recommended',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -23,6 +23,8 @@ module.exports = {
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
+    'indent': 0,
+    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
