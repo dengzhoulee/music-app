533f9fb061616ca37e0578b7da15499a4e3d21c8
l-d-z <dengzhouli@163.com>
2018-03-14 17:01:05 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 import router from './router'
 import FastClick from 'fastclick'
 import 'common/style/index.sass'
@@ -11,6 +12,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   render: h => h(App)
 })
